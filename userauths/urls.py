cfd135c6e26edc127717e5c31cab26bb486b007e
michaelangelo <nanantwiozone@gmail.com>
2024-08-10 03:17:33 +0000

    signin/signup

--- userauths/urls.py
+++ userauths/urls.py
@@ -6,6 +6,6 @@ app_name = "userauths"
 
 urlpatterns = [
     path("sign-up/", views.RegisterView, name="sign-up"),
-    path("sign-in/", views.RegisterView, name="sign-in"),
-    path("sign-out/",views.RegisterView, name="sign-out")
+    path("sign-in/", views.LoginView, name="sign-in"),
+    path("sign-out/",views.logoutView, name="sign-out")
 ]
